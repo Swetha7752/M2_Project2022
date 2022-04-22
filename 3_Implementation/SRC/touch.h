054f5994cafd3870f21ec537d8f7febb7729e10b
Swetha7752 <102161726+Swetha7752@users.noreply.github.com>
2022-04-22 14:36:58 +0530

    Add files via upload

---
+++ 3_Implementation/SRC/touch.h
@@ -0,0 +1,20 @@
+
+HEADER FILE
+
+#ifndef touch.h
+
+#define touch.h
+
+#define F_CPU 1000000
+
+#define E 5
+
+#define RS 6
+
+void send_a_command(unsigned char command);
+
+void send_a_character(unsigned char character);
+
+void send_a_string(char *string_of_characters);
+
+#endif
